f87c427917d035c292fefe260c6e1af9eb853b32
kobbybawuah <Bawuah10@hotmail.com>
2023-07-28 21:03:33 +0000

    Functional UI built to take PDF and text files

--- GUI.py
+++ GUI.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import openai
 from colorama import Fore, Back, Style
 import sentry_sdk
+import streamlit as st
+from PyPDF2 import PdfReader
 
 sentry_sdk.init(
   dsn="https://5a87e033b27340fe82b9c0a6a0eb93fc@o1145044.ingest.sentry.io/4505588043284480",
@@ -80,50 +82,81 @@ def get_response(index, previous_questions_and_answers, new_question):
 #If you want to use outside data in conjuction with the internal data, pass in a language model like this
 #print(index.query(query,llm=ChatOpenAI()))
 #Current implimentation should only send the standalone question to vector store as oposed to the LLM model. 
-  result = index.query(messages)
-
+  # result = index.query(messages)
+  result = "Demo data"
   # print("Response: -------->",result)
 
   return result
 
 def main():
-  #clear the terminal on run
-  os.system("cls" if os.name == "nt" else "clear")
+  #UI
+    st.set_page_config(page_title="Ask your File")
+    st.header("Ask your PDF or Text file 💬")
 
-  previous_questions_and_answers = ""
+    file_type = ["pdf", "txt"]
+    text_content = ""
 
-  #Text loader
-  loader = TextLoader('constitution.txt')
-  # print ("rahh 1")
+    #upload the file
+    uploaded_file = st.file_uploader("Upload your PDF or Text", type=file_type)
 
-  # loader = DirectoryLoader(".", glob="*.txt")
-  index = VectorstoreIndexCreator().from_loaders([loader])
-  # print ("rahh 2")
+    if uploaded_file is not None:
+        file_extension = uploaded_file.name.split(".")[-1]
+        if file_extension.lower() == "pdf":
+            # Handle PDF file
+            st.write("You uploaded a PDF file!")
+            # Process the PDF file 
+            pdf_reader = PdfReader(uploaded_file)
+            for page in pdf_reader.pages:
+                text_content += page.extract_text()
+            st.write(text_content)
 
-  while True:
-    # ask the user for their question
-    new_question = input(
-        Fore.GREEN + Style.BRIGHT + "How can I help you?: " + Style.RESET_ALL
-      )
-    # check the question is safe
-    errors = get_moderation(new_question)
-    if errors:
-            print(
-                Fore.RED
-                + Style.BRIGHT
-                + "Sorry, you're question didn't pass the moderation check:"
-            )
-            for error in errors:
-                print(error)
-            print(Style.RESET_ALL)
-            continue
+        elif file_extension.lower() == "txt":
+            # Handle text file
+            st.write("You uploaded a text file.")
+            # Process the text file 
+            text_content = uploaded_file.getvalue().decode("utf-8")
+            st.write(text_content)
+
+        else:
+            st.error("Unsupported file type! Please upload a PDF or text file.")
+
+    previous_questions_and_answers = ""
+
+    # #Text loader
+    # loader = TextLoader('constitution.txt')
+    # # print ("rahh 1")
+
+    # # loader = DirectoryLoader(".", glob="*.txt")
     
-    response = get_response(index, previous_questions_and_answers, new_question)
+    
+    
+    # index = VectorstoreIndexCreator().from_loaders([text_content])
+    # st.write("vector created!!!")
 
-    previous_questions_and_answers+="Human: " + new_question + " " + "AI: " + response + " "
+    # while True:
+    #     # ask the user for their question
+    #     new_question = input(
+    #         Fore.GREEN + Style.BRIGHT + "How can I help you?: " + Style.RESET_ALL
+    #     )
+    #     # check the question is safe
+    #     errors = get_moderation(new_question)
+    #     if errors:
+    #             print(
+    #                 Fore.RED
+    #                 + Style.BRIGHT
+    #                 + "Sorry, you're question didn't pass the moderation check:"
+    #             )
+    #             for error in errors:
+    #                 print(error)
+    #             print(Style.RESET_ALL)
+    #             continue
+        
+    #     response = get_response(index, previous_questions_and_answers, new_question)
 
-    # print the response
-    print(Fore.CYAN + Style.BRIGHT + "Here you go: " + Style.NORMAL + response)
+    #     previous_questions_and_answers+="Human: " + new_question + " " + "AI: " + response + " "
+
+    #     # print the response
+    #     print(Fore.CYAN + Style.BRIGHT + "Here you go: " + Style.NORMAL + response)
 
 if __name__ == "__main__":
     main()
